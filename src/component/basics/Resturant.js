8bbc936c2c45c14ed7ebb7f3942b56f91c99d8cb
adarshbraddi@gmail.com <adarshbraddi@gmail.com>
2023-05-19 12:30:01 +0530

    Recat Project for Resturent

---
+++ src/component/basics/Resturant.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import "./style.css";
+import Menu from "./menuApi.js";
+import MenuCard from './MenuCard';
+
+const uniqueList = [ ...new Set ( 
+  Menu.map((curElem) => {
+ return curElem.category;
+})
+),
+];
+  console.log(uniqueList);
+
+
+ const Resturant = () => {
+
+    const [menuData, setMenudata] = useState(Menu);
+
+    const filterItem = (category) => {
+       const updatedList = Menu.filter((curElem) => {
+        return curElem.category === category;
+       });
+       setMenudata(updatedList);
+    };
+     
+  return (
+    <> 
+
+    <nav className='navbar'>
+      <div className='btn-group'>
+        <button className='btn-group__item' onClick={() => filterItem("breakfast")}>BreakFast</button>
+        <button className='btn-group__item'  onClick={() => filterItem("lunch")}>Lunch</button>
+        <button className='btn-group__item'  onClick={() => filterItem("evening")}>Evening</button>
+        <button className='btn-group__item'  onClick={() => filterItem("dinner")}>Dinner</button>
+        <button className='btn-group__item'  onClick={() => setMenudata(Menu)}>All</button>
+      </div>
+    </nav>
+    <MenuCard menuData = {menuData} />
+    </>
+  );
+};
+
+export default Resturant;
